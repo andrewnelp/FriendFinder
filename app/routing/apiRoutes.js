dfb8fd45861404f13c39418f3774fa944231a7ca
Andrey Nelepov <nelepow@gmail.com>
2019-05-20 20:28:52 -0400

    added popup, needs fix

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,42 +9,8 @@ module.exports = function (app) {
 
   //posting data to apis
   app.post('/api/friends', function (req, res) {
-    let totalArr = [];
-    let friendsArrScores = [];
     friendsData.push(req.body);
     res.json();
-    console.log(friendsArrScores);
-    // transfer arrays to numbers
-    // let mineScores = friendsData.slice(-1).scores.map(Number);
-    // let newFriend = req.body.slice(-1);
-    console.log(mineScores);
-
-    // for (let i = 0; i < friendsArr.length; i++) {
-    //   friendsArrScores = friendsArr[i].scores.map(Number);
-    //   console.log(friendsArrScores);
-    //   let totalDiff = 0;
-    //   for (let i = 0; i < newFriendScores.length; i++) {
-
-    //     // finding the total difference sum
-    //     totalDiff += Math.abs(newFriendScores[i] - friendsArrScores[i]);
-    //   }
-    //   console.log(totalDiff);
-
-    //   // pushing totalDiff to totalArr and then need to find min number
-    //   totalArr.push(totalDiff);
-    // }
-    // console.log(totalArr);
-    // //now we need to find a min number in totalDiff array and
-    // //calculate min number and find its index which is going to equal the best friend
-    // indexBestFriend = totalArr.indexOf(Math.min.apply(null, totalArr));
-    // console.log(indexBestFriend);
-    // matchedFriendName = friendsArr[indexBestFriend].name;
-    // matchedFriendPhoto = friendsArr[indexBestFriend].photo;
-    // console.log(matchedFriendName);
-    // console.log(matchedFriendPhoto);
-    // friendsData.push(req.body);
-    // res.json(true);
   });
-
 
 }
