a93f92bb3378e45e4a6e9016f26622c6b5cf7425
Andrey Nelepov <nelepow@gmail.com>
2019-05-30 20:25:55 -0400

    added es6

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,12 @@ let friendsData = require('../data/friends-data.js');
 
 module.exports = function (app) {
   //getting data from server
-  app.get('/api/friends', function (req, res) {
-    return res.json(friendsData);
+  app.get('/api/friends', (req, res) => {
+    return res.json(friendsData)
   });
 
   //posting data to apis
-  app.post('/api/friends', function (req, res) {
+  app.post('/api/friends', (req, res) => {
     friendsData.push(req.body);
     res.json();
     // res.redirect("/survey");
